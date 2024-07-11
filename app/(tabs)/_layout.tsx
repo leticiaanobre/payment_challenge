ef8da8a14691477d585140be004e77fd28aaa707
Leticia Nobre <leticiavidal2910@gmail.com>
2024-07-11 11:35:43 -0300

    🔥 refat: removed files and adjusted code

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -15,29 +15,11 @@ export default function TabLayout() {
         headerShown: false,
       }}>
       <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
         name="PaymentMethodScreen"
         options={{
           title: 'Payment',
           tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
+            <TabBarIcon name={focused ?  'home' : 'home-outline'} color={color} />
           ),
         }}
       />
